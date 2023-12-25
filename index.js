6a0c99d8e7d75a4c3ca9e1801096240480fd189a
digvijaysingh21 <singh.v.digvijay@gmail.com>
2023-12-25 15:58:23 +0530

    rock paper scissor game

---
+++ index.js
@@ -0,0 +1,47 @@
+const buttons = document.querySelectorAll('button');
+
+const resultElement = document.getElementById("result");
+
+const playerScoreElement = document.getElementById("user-score");
+
+const computerScoreElement = document.getElementById("computer-score");
+
+let playerScore = 0;
+
+let computerScore = 0;
+
+
+buttons.forEach(button=>{
+  button.addEventListener('click', () =>{
+    // console.log('user choice', button.id,
+    // "computer choice", computerPlay());
+
+    const result = playRound(button.id, 
+      computerPlay());
+      resultElement.textContent = result;
+  });
+});
+
+function computerPlay(){
+  const choices = ["rock", "paper", "scissors"];
+  const randomChoice = Math.floor(Math.random() * choices.length);
+  return choices[randomChoice];
+}
+
+function playRound(playerSelection, computerSelection){
+  if(playerSelection === computerSelection){
+    return "It is a tie!";
+  }else if(
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === 'paper' && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ){
+    playerScore++;
+    playerScoreElement.textContent = playerScore;
+    return "You win! " + playerSelection + " beats " + computerSelection;
+  }else{
+    computerScore++;
+    computerScoreElement.textContent = computerScore;
+    return "You lose! " + computerSelection + " beats " + playerSelection;
+  }
+}
